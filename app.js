db3ee0ea199eae9dce327a77b4acdd9ab0266e83
Acambaro <galeana508@hotmail.com>
2016-02-15 13:11:11 -0800

    problem6

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 //You'll need one of these for each answer
 var pTagOne = document.getElementById('ans-one');
+var pTagTwo = document.getElementById('ans- two');
+var pTagThree = document.getElementById('ans-three');
+var pTagFour = document.getElementById('ans-four');
+var pTagFive = document.getElementById('ans-five');
+var pTagsix = document.getElementById('ans-six');
 
-var ans1 = prompt('Did Sam live in South Dakota for 8 years?');
-if (ans1.toLowerCase() === 'n' || ans1.toUpperCase() === 'NO') {
-  // alert('You\'re right! Sam never lived in South Dakota!');
-  // The alert() is replaced with the following line
-  pTagOne.textContent = 'You\'re right! Sam never lived in South Dakota!';
-} else if (ans1.toLowerCase() === 'y' || ans1.toUpperCase() === 'YES') {
-  // alert('Nope! Sam never lived in South Dakota!');
-  // The alert() is replaced with the following line
-  pTagOne.textContent = 'Nope! Sam never lived in South Dakota!';
-} else {
-  // This error-correction can stay as an alert
-  alert('Please give a yes or no answer.');
-}
+// var ans1 = prompt('Did Sam live in South Dakota for 8 years?');
+// if (ans1.toLowerCase() === 'n' || ans1.toUpperCase() === 'NO') {
+//   // alert('You\'re right! Sam never lived in South Dakota!');
+//   // The alert() is replaced with the following line
+//   pTagOne.textContent = 'You\'re right! Sam never lived in South Dakota!';
+// } else if (ans1.toLowerCase() === 'y' || ans1.toUpperCase() === 'YES') {
+//   // alert('Nope! Sam never lived in South Dakota!');
+//   // The alert() is replaced with the following line
+//   pTagOne.textContent = 'Nope! Sam never lived in South Dakota!';
+// } else {
+//   // This error-correction can stay as an alert
+//   alert('Please give a yes or no answer.');
+// }
 function sum(n1 , n2){
   var total = (n1 + n2);
-  console.log ('The sum of ' + n1 + ' and '+ n2 + ' is ' + total + '.' );
+  // console.log ('The sum of ' + n1 + ' and '+ n2 + ' is ' + total + '.' );
+  pTagOne.textContent = 'The sum of ' + n1 + ' and '+ n2 + ' is ' + total + '.' ;
   return total;
 }
 function multiply(n1 , n2){
@@ -34,7 +40,7 @@ function sumAndMultiply(n1,n2,n3){
   console.log(' The numbers ' + n1 + ' and ' + n2 + ' and ' + n3 + ' have a product of ' + multiplyTotal + '.');
 
   var answerArray = [sumTotal,multiplyTotal];
-
+  PtagTwo.textContent = n1 + ' and '+ n2 + ' and ' + n3 + ' sum to ' + sumTotal + '.';
   return answerArray;
 }
 
